35e1abba935e9c2f6790a6337a9a954b6c0e8a34
Maciej Małecki <malecki.maciej@gmail.com>
2021-02-12 13:21:54 +0100

    Update check.sh

--- check.sh
+++ check.sh
@@ -8,7 +8,7 @@ wget --quiet http://theweb.dk/KickAssembler/_FrontPage.html
 VERSION=`cat _FrontPage.html | grep -Po '(?<=Kick Assembler V)\d+.\d.'`
 echo "Found version ${VERSION}"
 
-HTTP_CODE=`curl -s -o /dev/null -I -w "%{http_code}" https://github.com/c64lib/asm-ka/releases/tag/${VERSION}`
+HTTP_CODE=`curl -s -o /dev/null -I -w "%{http_code}" https://github.com/maciejmalecki/asm-ka/releases/tag/${VERSION}`
 echo "Version ${VERSION} released?: ${HTTP_CODE}"
 
 if [ "$HTTP_CODE" == "404" ]
